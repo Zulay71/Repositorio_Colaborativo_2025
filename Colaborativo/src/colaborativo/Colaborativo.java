68d0b0b47bb976ffae3cf5d97012923162ef62f3
Zulay71 <zulaybustos71@gmail.com>
2025-09-02 10:45:34 -0600

    Arch Zulay

---
+++ Colaborativo/src/colaborativo/Colaborativo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package colaborativo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author luzma
+ */
+public class Colaborativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ Scanner sc = new Scanner(System.in);
+
+ System.out.print("Ingresa el primer numero: ");
+ int numero1 = sc.nextInt();
+
+ System.out.print("Ingresa el segundo numero: ");
+ int numero2 = sc.nextInt();
+
+ int suma = numero1 + numero2;
+
+System.out.println("La suma es: " + suma);
+}     
+}
+    
+    
+
